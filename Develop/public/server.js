ac98a112ae47f152b62879818c9049b9eda4d31e
Robert Candler <robert.candler.32@gmail.com>
2020-04-19 11:22:41 -0600

    partial posting now

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -2,33 +2,63 @@
 var http = require("http");
 var fs = require("fs");
 
-// Set our port to 8080
+// Set our port to 7070
 var PORT = 7070;
 
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
 
-  // Capture the url the request is made to
+  // path is url
   var path = req.url;
 
-  // When we visit different urls, call the function with different arguments
+  // switch between index and notes page
   switch (path) {
 
   case "/notes":
     return renderHTML(path + ".html", res);
+  case "/returned":
+      return renderNote(req, res);
 
   default:
     return renderHTML("/index.html", res);
   }
 }
 
-// function to take a filepath and respond with html
+// function chooses to put out html pages
 function renderHTML(filePath, res) {
   return fs.readFile(__dirname + filePath, function(err, data) {
     if (err) throw err;
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
+  });
+}
+
+//function saves input text as note in db
+function renderNote(req, res) {
+  // Saving the request posted data as a variable.
+  var requestData = "";
+
+  var myHTML =
+    "<html><head><title>Something Went Wrong!</title></head><body><h1>Data has not been received!</h1></body></html>";
+
+  // When the server receives data, it will add it to requestData.
+  req.on("data", function(data) {
+    requestData += data;
+    console.log("You just posted some data to the server:\n", requestData);
+
+    myHTML =
+      "<html><head><title>Hello Noder!</title></head><body>" +
+      "<h1>Thank you for the data: </h1><code>" +
+      requestData +
+      "</code>" +
+      "</body></html>";
+  });
+
+  // When the request has ended...
+  req.on("end", function() {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(myHTML);
   });
 }
 
